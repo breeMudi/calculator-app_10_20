aca828b2a9665e1ee00ea8682202fae04cde08d3
Bridget Mudiaga-Ojemu <65234249+breeMudi@users.noreply.github.com>
2023-06-08 21:01:28 +0200

    Add files via upload
    calculator app files

---
+++ calculator-app-main/main.js
@@ -0,0 +1,15 @@
+const theme = {
+    first: {bg: ['#3a4764', '#232c43', '#182034'],
+    keys: ['#637097', '#404e72', '#d03f2f', '#93261a', '#eae3dc', '#b4a597'],
+    text: ['#444b5a', '#ffffff']},
+
+    second: {bg: [],
+    keys: [],
+    text: []},
+
+    third: {bg: [],
+    keys: [],
+    text: []}
+}
+
+console.log('the hex code for first key text is ' + theme.first.text[1])

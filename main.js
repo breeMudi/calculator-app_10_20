959417eeeb97c2aecc7bd42bb194a8cd6dcaa9d3
Bridget Mudiaga-Ojemu <65234249+breeMudi@users.noreply.github.com>
2023-06-22 01:02:35 +0200

    Add files via upload
    re-upload files to root

---
+++ main.js
@@ -0,0 +1,120 @@
+const theme = {
+    one: {bg: ['#3a4764', '#232c43', '#182034'],
+    keys: ['#637097', '#404e72', '#d03f2f', '#93261a', '#eae3dc', '#b4a597'],
+    text: ['#ffffff', '#444b5a', "#ffffff"],
+    hover: ['#ffffff', '#a2b3e1', '#f96c5b']},
+
+    two: {bg: ["#e6e6e6", '#d1cccc', "#ededed"],
+    keys: ["#377f86", "#1b5f65", "#ca5502", "#893901", "#e5e4e1", "#a69d91" ],
+    text: ["#35352c",,"#35352c", "#ffffff"],
+    hover: ['#ffffff', '#62b5bd', '#ff8b38']},
+
+    three: {bg: ["#160628", "#1d0934", "#1d0934"],
+    keys: ["#58077d", "#bc15f4", "#00e0d1", "#6cf9f2", "#341c4f", "#871c9c"],
+    text: ["#ffe53d", "#ffe53d", "#1b2428"],
+    hover: ['#6b34ac', '#8631b0', '#94fff9']}
+}
+
+// const floatPosition = {one: "5px", two: "29px", three: "58px"}
+const floatPosition =[ "5px", "29px", "58px"]
+
+function defineTheme (num) {
+    let outlet
+    if (num===0){outlet = theme.one}
+    else if (num===1){outlet = theme.two}
+    else if (num===2){outlet = theme.three}
+    return outlet
+}
+// console.log('the hex code for first key text is ' + theme.first.text[1])
+// const themes = document.querySelector('.slider button')
+
+
+
+// function resetSliderColor () {
+//     themes.style.backgroundColor = 'transparent'
+// }
+// instead of making everything transparent and only the theme in focus to have a color
+// we are going to change the relative position of a single node
+// probably using the ForEach method
+// better still. create another div called floater, and make it move depending on the button you press
+
+function changeTheme (numbers) {
+    const selectedTheme = defineTheme(numbers[0])
+    let root = document.documentElement.style
+    // set BG properties
+    root.setProperty("--body-bg", selectedTheme.bg[0])
+    root.setProperty("--keypad-bg", selectedTheme.bg[1])
+    root.setProperty("--display-bg", selectedTheme.bg[2])
+    // set key Properties
+    root.setProperty("--del-reset-bg", selectedTheme.keys[0])
+    root.setProperty("--del-reset-shadow", selectedTheme.keys[1])
+    root.setProperty("--equal-bg", selectedTheme.keys[2])
+    root.setProperty("--equal-shadow", selectedTheme.keys[3])
+    root.setProperty("--other-key-bg", selectedTheme.keys[4])
+    root.setProperty("--other-key-shadow", selectedTheme.keys[5])
+    // set text Properties
+    root.setProperty("--display-header-color", selectedTheme.text[0])
+    root.setProperty("--key-color", selectedTheme.text[1])
+    root.setProperty("--equal-color", selectedTheme.text[2])
+    // set hover Properties
+    root.setProperty("--hover-number", selectedTheme.hover[0])
+    root.setProperty("--hover-dr", selectedTheme.hover[1])
+    root.setProperty("--hover-equal", selectedTheme.hover[2])
+    
+    document.querySelector('.floater').style.left = floatPosition[numbers[0]]
+    // resetSliderColor()
+    // document.querySelector(':root').style
+    // console.log(document.querySelector(':root'))
+}
+// UPDATE DISPLAY VALUES...
+
+// let values = document.querySelector(".values")
+// const keys = document.querySelector(".row")
+// const seven = document.querySelector(".seven")
+// seven.addEventListener('click', () => {
+//     const valTxt = document.createTextNode(seven.value)
+//     values.appendChild(valTxt)
+// })
+
+// const equal = document.querySelector('#equal')
+// equal.addEventListener('click', () => {
+//     document.querySelector('.solution').textContent = (values.textContent)
+// })
+
+
+// CALCULATION SECTION
+
+let solution = document.querySelector('.solution')
+const number = document.querySelectorAll('.number')
+let typed = document.querySelector('.typed p')
+let calculation = document.querySelector('#equal')
+let toCalc
+// let val = values.createElement
+
+// number.forEach(num => (console.log(num)))
+number.forEach(num => num.addEventListener('click', () => {
+    let t = document.createTextNode(num.value)
+    typed.appendChild(t)
+    // solution.textContent=Math.eval (typed.textContent)
+}))
+
+calculation.addEventListener('click', () => {
+    toCalc = typed.textContent.replace('X', '*')
+    try {answer = eval(toCalc)}
+    catch {answer = 'ERR'}
+    // console.log(typeof(answer))
+    if(String(answer).length > 10){solution.textContent = answer.toFixed(10)}
+    else {solution.textContent = answer}
+})
+
+function reset (){
+    typed.textContent=''
+    solution.textContent=0
+}
+
+function del (){
+    let input = typed.textContent
+    input = input.slice(0, -1)
+    // console.log(input)
+    typed.textContent=input
+}
---
+++ style-guide.md
@@ -0,0 +1,93 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Theme 1
+
+#### Backgrounds
+
+- Very dark desaturated blue (main background ✅): hsl(222, 26%, 31%) #3a4764
+- Very dark desaturated blue (toggle background, keypad background ✅): hsl(223, 31%, 20%) #232c43
+- Very dark desaturated blue (screen background): hsl(224, 36%, 15%) #182034✅
+
+#### Keys
+
+- Desaturated dark blue (key background): hsl(225, 21%, 49%) #637097✅
+- Desaturated dark blue (key shadow): hsl(224, 28%, 35%) #404e72
+
+- Red (key background, toggle): hsl(6, 63%, 50%) #d03f2f ✅
+- Dark red (key shadow): hsl(6, 70%, 34%) #93261a
+
+- Light grayish orange (key background): hsl(30, 25%, 89%) #eae3dc ✅
+- Grayish orange (key shadow): hsl(28, 16%, 65%) #b4a597
+
+#### Text
+
+- Very dark grayish blue: hsl(221, 14%, 31%) #444b5a ✅
+- White: hsl(0, 0%, 100%) #ffffff ✅
+
+### Theme 2
+
+#### Backgrounds
+
+- Light gray (main background): hsl(0, 0%, 90%) #e6e6e6
+- Grayish red (toggle background, keypad background): hsl(0, 5%, 81%) #d1cccc
+- Very light gray (screen background): hsl(0, 0%, 93%) #ededed
+
+#### Keys
+
+- Dark moderate cyan (key background): hsl(185, 42%, 37%)#377f86
+- Very dark cyan (key shadow): hsl(185, 58%, 25%) #1b5f65
+
+- Orange (key background, toggle): hsl(25, 98%, 40%) #ca5502
+- Dark orange (key shadow): hsl(25, 99%, 27%) #893901
+
+- Light grayish yellow (key background): hsl(45, 7%, 89%) #e5e4e1
+- Dark grayish orange (key shadow): hsl(35, 11%, 61%) #a69d91
+
+#### Text
+
+- Very dark grayish yellow: hsl(60, 10%, 19%) #35352c
+- White (text): hsl(0, 0%, 100%) #ffffff
+
+### Theme 3
+
+#### Backgrounds
+
+- Very dark violet (main background): hsl(268, 75%, 9%) #160628
+- Very dark violet (toggle background, keypad background, screen background): hsl(268, 71%, 12%) #1d0934
+
+#### Keys
+
+- Dark violet (key background): hsl(281, 89%, 26%) #58077d
+- Vivid magenta (key shadow): hsl(285, 91%, 52%) #bc15f4
+
+- Pure cyan (key background, toggle): hsl(176, 100%, 44%) #00e0d1
+- Soft cyan (key shadow): hsl(177, 92%, 70%) #6cf9f2
+
+- Very dark violet (key background): hsl(268, 47%, 21%) #341c4f
+- Dark magenta (key shadow): hsl(290, 70%, 36%) #871c9c
+
+#### Text
+
+- Light yellow: hsl(52, 100%, 62%) #ffe53d
+- White (text): hsl(0, 0%, 100%) #ffffff
+- Very dark blue: hsl(198, 20%, 13%) #1b2428
+
+## Typography
+
+### Body Copy
+
+- Font size (numbers): 32px
+
+### Font
+
+- Family: [League Spartan](https://fonts.google.com/specimen/League+Spartan)
+- Weights: 700
